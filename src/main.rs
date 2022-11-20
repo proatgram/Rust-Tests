e4c0221c6797bc64428e1751769a96ebd7c60ee2
Timothy Hutchins <tgh6407@gmail.com>
2022-11-19 22:14:46 -0600

    hehe

---
+++ src/main.rs
@@ -0,0 +1,32 @@
+fn main() {
+    println!("Guess the number!");
+    loop {
+        println!("Input your guess: ");
+
+        let mut guess = String::new();
+
+        let random_num = rand::random::<u32>();
+
+        println!("{random_num}");
+
+        std::io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+
+        println!("Your Guess: {guess}");
+
+        let guess: u32 = guess.trim()
+            .parse::<u32>()
+            .expect("Please type a number.");
+
+        match guess.cmp(&random_num) {
+            std::cmp::Ordering::Less => println!("HAHA TOO SMALL DING NUT!"),
+            std::cmp::Ordering::Greater => println!("HAHA TOO BIG BIGGER DING NUT!"),
+            std::cmp::Ordering::Equal =>  {
+                println!("okayy you got it. good job.");
+                break;
+            }
+        }
+    }
+    return;
+}
